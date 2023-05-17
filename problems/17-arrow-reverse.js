1ee1beab7f11a9a1c9817e77c7b88443f1780d18
arjunnbhujel <arjunbhujel600@gmail.com>
2023-05-17 08:33:55 +0545

    Closures practice of JS

---
+++ problems/17-arrow-reverse.js
@@ -0,0 +1,31 @@
+/***********************************************************************
+Write a function reverseStr(str) that accepts a string and returns that string
+reversed.
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = reverseStr("hello"); // returns "olleh"
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
+
+
+***********************************************************************/
+
+// Your code here
+
+
+let reverseStr = (str) => {
+  let finalWord = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    finalWord = finalWord.concat(str[i]);
+  }
+  return finalWord;
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+    module.exports = reverseStr;
+  } catch (e) {
+    return null;
+  }
